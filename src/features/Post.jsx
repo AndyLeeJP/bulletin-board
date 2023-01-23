989a8a5956a94950739d6268492d615f58fb4af0
vaperLee <rize1126jesse@gmail.com>
2023-01-23 23:30:35 +0900

    setup for redux

---
+++ src/features/Post.jsx
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { PostsData } from "../DummyData";
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState: { value: PostsData },
+  reducers: {
+    addPost: (state, action) => {
+      state.value.push(action.payload);
+    },
+  },
+});
